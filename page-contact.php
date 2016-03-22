1f5b531124a72bdc9b2b76c03b71823031774b66
jianganglu <jianganglu@hengtiansoft.com>
2016-03-22 17:39:58 +0800

    Initial Commit.

---
+++ page-contact.php
@@ -0,0 +1,32 @@
+﻿<?php
+/**
+ * Template Name: Custom Contact Form
+ */
+
+get_header(); ?>
+
+联系我们
+
+<?php
+  if(have_posts()) : 
+    while(have_posts()) : the_post();
+      the_content();
+    endwhile;
+  endif;
+?>
+
+<div id="main">
+
+<h2>Archives by Month:</h2>
+  <ul>
+    <?php wp_get_archives('type=monthly'); ?>
+  </ul>
+
+<h2>Archives by Subject:</h2>
+  <ul>
+     <?php wp_list_cats(); ?>
+  </ul>
+
+</div>
+
+<?php get_footer(); ?>
