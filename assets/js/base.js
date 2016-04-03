f5394bb0a3712359475d995e66b89c8c96a3d896
jianganglu <1055375563@qq.com>
2016-04-03 21:25:25 +0800

    update

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,12 +1,18 @@
-﻿requirejs.config({
-  baseUrl: '.',
-  paths: {
-    jquery: 'assets/vendor/jquery/jquery',
-    bootstrap: 'assets/vendor/bootstrap/js/bootstrap'
-  },
-  shim: {
-    bootstrap: {
-      deps: 'jquery'
+requirejs.config({
+    baseUrl: '.',
+    paths: {
+        jquery: 'assets/vendor/jquery/jquery',
+        bootstrap: 'assets/vendor/bootstrap/js/bootstrap',
+        isotope: 'assets/vendor/isotope/isotope.pkgd',
+        bridget: 'assets/vendor/jquery-bridget/jquery-bridget',
+        carousel: 'assets/vendor/owl-carousel2/owl.carousel'
+    },
+    shim: {
+        bootstrap: ['jquery'],
+        isotope: ['jquery'],
+        carousel: ['jquery']
     }
-  }
-})
+});
+define('./assets/js/custom/base', [], function () {
+    return;
+});
